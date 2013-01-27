7f66164a37723fa21fd38f1a605fb650188f881f
Rob <lysergic_evangelion@live.nl>
2013-01-27 14:25:01 +0100

    Update src/com/viish/apps/tripletriad/settings/SettingsActivity.java
    updated code comments

--- src/com/viish/apps/tripletriad/settings/SettingsActivity.java
+++ src/com/viish/apps/tripletriad/settings/SettingsActivity.java
@@ -1,14 +1,17 @@
+// import game specific packages
 package com.viish.apps.tripletriad.settings;
 
+// import java packages
 import java.util.List;
 
+// import android packages
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
 import com.viish.apps.tripletriad.R;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity {    
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.preference_headers, target);
